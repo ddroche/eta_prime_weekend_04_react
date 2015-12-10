25ded37539a402ffc93f93f4c42dd92d8d07fed3
Derek Roche <ddrochedsp@gmail.com>
2015-12-10 10:29:55 -0600

    Searching produces results to screen

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
       var data = $(this).serializeArray();
       console.log(data);
-      data.forEach(function(elem){
+      data.forEach(function(elem) {
         if(elem.value != '') {
           data[elem.name] = elem.value;
         }
@@ -19,7 +19,7 @@ $(document).ready(function() {
         method: 'POST',
         url: '/apply',
         data: data
-      }).done(function(){
+      }).done(function() {
         alert('Thanks for your submission!');
       });
 
@@ -35,13 +35,59 @@ $(document).ready(function() {
       $.ajax({
         method: 'GET',
         url: '/admin/search/' + $('#searchbox').val()
-      }).done(function(data){
+      }).done(function(applications) {
         console.log('button works');
-        console.log(data);
+        console.log(applications);
+        renderApps(applications);
       });
     } catch (exception) {
       console.log(exception);
     }
-  })
+  });
 
 });
+
+function renderApps(applications) {
+
+  var $tbody = $('<tbody>');
+  var $theader = $('<tr>');
+  var $firstNameHead = $('<th>').html('First Name');
+  var $lastNameHead = $('<th>').html('Last Name');
+  var $desiredLocationHead = $('<th>').html('Desired Location');
+  var $lastEmployerHead = $('<th>').html('Last Employer');
+  var $skillsHead = $('<th>').html('Skills');
+  var $dateAppliedHead = $('<th>').html('Date Applied');
+
+  $theader.append($firstNameHead);
+  $theader.append($lastNameHead);
+  $theader.append($desiredLocationHead);
+  $theader.append($lastEmployerHead);
+  $theader.append($skillsHead);
+  $theader.append($dateAppliedHead);
+
+  $tbody.html($theader);
+
+
+
+  applications.forEach(function(application) {
+    var $tr = $('<tr>');
+    var $firstName = $('<td>').html(application.firstName);
+    var $lastName = $('<td>').html(application.lastName);
+    var $desiredLocation = $('<td>').html(application.desiredLocation);
+    var $lastEmployer = $('<td>').html(application.empHistory[0].employer);
+    var $skills = $('<td>').html(application.skills);
+    var $dateApplied = $('<td>').html(application.dateApplied);
+
+    $tr.append($firstName);
+    $tr.append($lastName);
+    $tr.append($desiredLocation);
+    $tr.append($lastEmployer);
+    $tr.append($skills);
+    $tr.append($dateApplied);
+
+    $tbody.append($tr);
+  });
+
+  $('#appTable').html($tbody);
+
+};
